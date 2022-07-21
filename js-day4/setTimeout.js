66d38e7bf1198488eb0a185b86ab6f55b57e65b4
wozaidf <2217957466@qq.com>
2022-07-21 13:03:56 +0800

    updata

--- js-day4/setTimeout.js
+++ js-day4/setTimeout.js
@@ -1,5 +1,15 @@
-setTimeout(function cbFn(){
-    console.log('learnInPro');
-}, 1000);
- 
-console.log('sync things');
+function fn2() {
+    console.log(1)
+    console.log(2)
+}
+fn2()                               //先出1后出2，表示同步
+
+
+function fn1() {
+    setTimeout(() => {
+        console.log(1)            //实现异步函数，将这个异步任务在同步任务后执行
+    })
+
+    console.log(2)                 //同步任务
+} 
+fn1()
--- js-day4/异步回调地狱.js
+++ js-day4/异步回调地狱.js
@@ -1,10 +1,34 @@
-    //回调地狱
-    setTimeout(function () {  //第一层
-        console.log('111');//等3秒打印111在执行下一个回调函数
-        setTimeout(function () {  //第二层
-            console.log('222');//等2秒打印222在执行下一个回调函数
-            setTimeout(function () {   //第三层
-                console.log('333');//等一秒打印333
-            }, 1000)
-        }, 2000)
-    }, 3000)
+//回调地狱
+setTimeout(function () {  //第一层
+    console.log('111');//等3秒打印111在执行下一个回调函数
+    setTimeout(function () {  //第二层
+        console.log('222');//等2秒打印222在执行下一个回调函数
+        setTimeout(function () {   //第三层
+            console.log('333');//等一秒打印333
+        }, 1000)
+    }, 2000)
+}, 3000)
+
+//例二          异步任务，回调函数  回调地狱
+function getTea(fn) {
+    setTimeout(() => {
+        fn('奶茶')
+    }, 2000)                 //异步任务
+}
+
+function getHotTop(fn) {
+    setTimeout(() => {
+        fn('火锅')
+    }, 2000)                 //异步任务
+}
+
+
+
+getHotTop(function fn(data) {
+    console.log(data)           //吃火锅
+    getTea(function fn(data) {
+        console.log(data)       //喝奶茶         //在异步任务中按我想要的顺序执行异步操作，就形成了回调地狱
+    })
+})
+
+console.log('不要等待直接')               //同步任务
